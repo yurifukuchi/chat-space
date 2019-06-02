23df404e832cab2b2232a1554a2ce8c40e4a2d16
yurifukuchi <uro_409@outlook.jp>
2019-06-02 21:22:31 +0900

    message model add

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
+    resources :messages, only: [:index, :create]
 end
