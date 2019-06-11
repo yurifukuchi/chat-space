d5e98cef9a3a3626024b0608f9dea01fa2a92ea5
yurifukuchi <uro_409@outlook.jp>
2019-06-12 00:48:14 +0900

    summary

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20190530164720) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "messages"
+    t.string   "content"
     t.string   "image"
     t.integer  "user_id"
     t.integer  "group_id"
