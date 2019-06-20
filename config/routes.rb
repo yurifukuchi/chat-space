3a8ea956ecf7d7b94b4c597adccbe7a3bf53aa14
yurifukuchi <uro_409@outlook.jp>
2019-06-21 01:58:00 +0900

    json.add

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+
+    namespace :api do
+      resources :message, only: :index, defaults: { format: 'json' }
+    end
   end 
 end
