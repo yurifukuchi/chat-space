fa7f1b96e46e44be0464e906637788ec6d2d6d01
yurifukuchi <uro_409@outlook.jp>
2019-06-22 05:17:20 +0900

    scss.fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :messages, only: [:index, :create]
 
     namespace :api do
-      resources :message, only: :index, defaults: { format: 'json' }
+      resources :messages, only: :index, defaults: { format: 'json' }
     end
   end 
 end
