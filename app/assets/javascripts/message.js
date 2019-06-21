2ceec87405a130b4ee84d6e2b085015a4c98b8bb
yurifukuchi <uro_409@outlook.jp>
2019-06-22 05:18:37 +0900

    autoupdate.add

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -11,18 +11,20 @@ $(function() {
 
     // var html = <div class='message'></div>
     // var myObj = $(".message");
+    var image = (message.image) ? `<img class="message__text__image" src="${message.image}">` : ""
     var html = `
-    <div class="message">
+    <div class="message" data-id=${message.id}>
     <div class="upper-info">
     <p class="upper-info__user">
     ${message.name}
     </p>
     <p class="upper-info__date">
-    ${message.data}
+    ${message.date}
     </p>
     </div>
     <p class="message__text">
-    ${message.content}    
+    ${message.content}
+    ${image}
     </p>
     </div>
     `
@@ -57,4 +59,38 @@ $(function() {
       $('.new-message__submit').prop('disabled', false);
     })
   })
+
+    
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var latest = $('.message:last');
+    last_message_id = latest.data('id');
+    var url = $(location).attr('pathname').replace("/message","/api/message");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: url,
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      messages.forEach(function(message) {
+        buildHTML(message);
+        $('.messages').animate({
+          scrollTop: $('.messages').get(0).scrollHeight
+        },100);
+        return false;
+      });
+
+      console.log('success');
+
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
+
